3dd420ab48aea4b0d474a58b6d5538673420c6d5
Eric Raio <ericraio@apple.com>
2023-09-20 06:35:51 -0700

    Show hidden files for neo-tree

--- lua/plugins/file-browser.lua
+++ lua/plugins/file-browser.lua
@@ -1,13 +1,33 @@
 return {
-  "nvim-telescope/telescope-file-browser.nvim",
-  keys = {
-    {
-      "<leader>sB",
-      ":Telescope file_browser path=%:p:h=%:p:h:<cr>",
-      desc = "Browse Files",
+  {
+    "nvim-telescope/telescope-file-browser.nvim",
+    keys = {
+      {
+        "<leader>sB",
+        ":Telescope file_browser path=%:p:h=%:p:h:<cr>",
+        desc = "Browse Files",
+      },
+    },
+    config = function()
+      require("telescope").load_extension("file_browser")
+    end,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_dotfiles = false,
+          hide_gitignored = true,
+          hide_by_name = {
+            ".git",
+            ".DS_Store",
+          },
+          never_show = {},
+        },
+      },
     },
   },
-  config = function()
-    require("telescope").load_extension("file_browser")
-  end,
 }
